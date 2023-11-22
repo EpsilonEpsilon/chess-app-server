bf04155d1a3fe00cd1e0ff99e069d9503844cc9f
Adoniev Ivan <adonjew.vaniya@gmail.com>
2023-11-23 01:29:19 +0200

    Custom js

--- dist/App.js
+++ dist/App.js
@@ -35,7 +35,7 @@ class App {
     }
     initRoutes() {
         this.app.use("/api", index_2.globalRouter);
-        this.app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(docs_1.openapiSpecification, { customCssUrl: CSS_URL }));
+        this.app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(docs_1.openapiSpecification, { customJs: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.10.3/swagger-ui.js" }));
         this.app.use(express_1.default.urlencoded({
             extended: true,
         }));
