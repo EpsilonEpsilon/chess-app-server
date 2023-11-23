ef7b4a873dba82e433ffbef88d7b9dce7d6451ed
Adoniev Ivan <adonjew.vaniya@gmail.com>
2023-11-23 16:14:01 +0200

    Docs changes

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -5,7 +5,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Hello World',
+            title: 'Express.js chess-server documentation',
             version: '1.0.0',
         },
         servers: [
